557f3f132e6d3220f91fe000de190fb116c06ab0
Ga2ng <gagangprakasa@gmail.com>
2025-07-29 03:36:56 +0700

    rollbak config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,27 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-    ],
-    dangerouslyAllowSVG: true,
-    contentDispositionType: 'attachment',
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-    formats: ['image/webp', 'image/avif', 'image/gif'],
-  },
-  experimental: {
-    optimizePackageImports: ['framer-motion'],
-  },
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.(gif|png|jpe?g|svg)$/i,
-      type: 'asset/resource',
-    });
-    return config;
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
