cf8504be833c44b4f64919136b649ef66bc9ebf1
Ga2ng <gagangprakasa@gmail.com>
2025-07-29 03:35:19 +0700

    change addfrtes gif and fix optimize gif

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+    dangerouslyAllowSVG: true,
+    contentDispositionType: 'attachment',
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    formats: ['image/webp', 'image/avif', 'image/gif'],
+  },
+  experimental: {
+    optimizePackageImports: ['framer-motion'],
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(gif|png|jpe?g|svg)$/i,
+      type: 'asset/resource',
+    });
+    return config;
+  },
+};
 
 export default nextConfig;
